9499bbf4b936d76d4d63fa3cad1460210cf6b693
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-08-15 13:09:29 -0300

    header-styled

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {React, useEffect, useState} from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeatureMovie from './components/FeatureMovie';
+import Header from './components/Header';
 import './App.css'
 // eslint-disable-next-line
 export default () =>{
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() =>{
     const loadAll = async () => {
@@ -15,6 +17,7 @@ export default () =>{
 
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+      console.log(list)
 
       //pegando o feature
 
@@ -26,11 +29,28 @@ export default () =>{
     }
 
     loadAll();
+  },[]);
 
-  },[])
+  useEffect(() =>{
+    const scrollListener = () => {
+        if(window.scrollY > 10){
+          setBlackHeader(true);
+        }else{
+          setBlackHeader(false);
+        }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () =>{
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, []);
 
   return (
     <div className="page">
+
+       <Header black={blackHeader}/>
 
       {featuredData &&
         <FeatureMovie item={featuredData}/>
@@ -41,6 +61,11 @@ export default () =>{
           <MovieRow key={key} title={item.title} items={item.items}/>
         ))}
       </section>
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤️</span> pela B7Web <br/>
+        Direitos de imagem para Netflix <br/>
+        Dados pegos do site Themoviedb.org
+      </footer>
     </div>
   )
 }
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -3,7 +3,6 @@ import './MovieRow.css';
 
 export default ({title, items}) =>{
     return(
-
         <div className="movieRow">
             <h2>{title}</h2>
             <div className="movieRow--listarea">
