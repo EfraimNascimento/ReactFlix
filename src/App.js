666966a0e756b6d6aa232cf9cae30ccf84e50393
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-08-17 12:51:17 -0300

    responsividade

--- src/App.js
+++ src/App.js
@@ -66,6 +66,13 @@ export default () =>{
         Direitos de imagem para Netflix <br/>
         Dados pegos do site Themoviedb.org
       </footer>
+
+       {movieList.length <= 0 &&
+        <div className="loading">
+            <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="loading page"></img>
+        </div>
+       }   
+
     </div>
   )
 }
